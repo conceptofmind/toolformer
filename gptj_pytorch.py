3531fc0f1b2caeb8bd9c86819bc2c3ad586a1bfd
Enrico Shippole <enricoship@gmail.com>
2023-02-17 00:45:44 -0500

    They round to 2 decimal places

--- gptj_pytorch.py
+++ gptj_pytorch.py
@@ -211,7 +211,6 @@ if __name__ == "__main__":
         dim_head = 64,
         heads = 8,
         ff_mult = 4,
-        dropout = 0.
     )
     tokens = torch.randint(0, 20000, (1, 512))
     logits = toolformer(tokens)
